0deb44d258c90b7a9fde03eac3b69a21ca27289a
Weronika <weronikalimberger@gmail.com>
2023-03-08 18:54:24 +0000

    Update joins

--- 7_joins.sql
+++ 7_joins.sql
@@ -10,3 +10,23 @@ select book_id, title, book.genre_id
 , genre.genre_id, name
 from book inner join genre
 on book.genre_id = genre.genre_id;
+
+select book_id, title, b.genre_id
+, g.genre_id, name
+from book as b inner join genre as g
+on b.genre_id = g.genre_id;
+
+select book_id, title, b.genre_id
+, g.genre_id, name
+from book as b left outer join genre as g
+on b.genre_id = g.genre_id;
+
+select book_id, title, b.genre_id
+, g.genre_id, name
+from book as b right outer join genre as g
+on b.genre_id = g.genre_id;
+
+select book_id, title
+, name
+from book as b inner join genre as g
+on b.genre_id = g.genre_id;
